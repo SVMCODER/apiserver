011ad7f442985cb1c4ee5093f685ed5268051825
svmcoder <124701282+SVMCODER@users.noreply.github.com>
2026-05-14 07:36:08 +0530

    Add API function that returns 500 error response

---
+++ netlify/functions/api.js
@@ -0,0 +1,15 @@
+exports.handler = async function(event, context) {
+    // Wait for 1 second (1000 milliseconds)
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    // Return the 500 Internal Server Error
+    return {
+        statusCode: 500,
+        headers: {
+            "Content-Type": "text/plain",
+            // Include CORS headers just in case you are calling this from a frontend
+            "Access-Control-Allow-Origin": "*", 
+        },
+        body: "Server returned Error, the API seems down"
+    };
+};
